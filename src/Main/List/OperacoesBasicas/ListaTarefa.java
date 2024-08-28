22e66d8d164af65fccdce31af6493482afec608c
Soren <danilo-258@hotmail.com>
2024-08-28 03:25:44 -0300

    first commit

---
+++ src/Main/List/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,50 @@
+package Main.List.OperacoesBasicas;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListaTarefa {
+  // atributo
+  private List<Tarefa> tarefaList;
+
+  public ListaTarefa() {
+    this.tarefaList = new ArrayList<>();
+  }
+
+  public void adicionarTarefa(String descricao) {
+    tarefaList.add(new Tarefa(descricao));
+  }
+
+  public void removerTarefa(String descricao) {
+    List<Tarefa> tarefasParaRemover = new ArrayList<>();
+    for (Tarefa t : tarefaList) {
+      if (t.getDescricao().equalsIgnoreCase(descricao)) {
+        tarefasParaRemover.add(t);
+      }
+    }
+    tarefaList.removeAll(tarefasParaRemover);
+  }
+
+  public int obeterNumeroTotalTarefas() {
+    return tarefaList.size();
+  }
+
+  public void obterDescricoesTarefas() {
+    System.out.print(tarefaList);
+  }
+
+  public static void main(String[] args) {
+    ListaTarefa listaTarefa = new ListaTarefa();
+    System.out.println("O numero total de elementos na lista é: " + listaTarefa.obeterNumeroTotalTarefas());
+
+    listaTarefa.adicionarTarefa("Tarefa 1");
+    listaTarefa.adicionarTarefa("Tarefa 1");
+    listaTarefa.adicionarTarefa("Tarefa 2");
+    System.out.println("O numero total de elementos na lista é: " + listaTarefa.obeterNumeroTotalTarefas());
+
+    listaTarefa.removerTarefa("Tarefa 2");
+    System.out.println("O numero total de elementos na lista é: " + listaTarefa.obeterNumeroTotalTarefas());
+
+    listaTarefa.obterDescricoesTarefas();
+  }
+}
---
+++ src/Main/List/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,22 @@
+package Main.List.OperacoesBasicas;
+
+public class Tarefa {
+  // atributo
+  private String descricao;
+
+  public Tarefa(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  @Override
+  public String toString() {
+    // TODO Auto-generated method stub
+    return "Tarefa{" +
+        "descrição='" + descricao + '\'' +
+        '}';
+  }
+}
